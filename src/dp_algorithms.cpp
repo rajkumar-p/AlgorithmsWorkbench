1b4a3bc41941a453772a49848f2a35a88212a23e
Rajkumar P <raj@diskodev.com>
2017-12-04 23:24:23 +0530

    Weighted Interval Scheduling Algorithm Impl
     Changes to be committed:
    	modified:   src/dp_algorithms.cpp
            - [Implementation of the weighted interval scheduling algorithm.
              Description of the problem is given in the .hpp file]
    	modified:   src/main.cpp
            - [Scrathpad]

--- src/dp_algorithms.cpp
+++ src/dp_algorithms.cpp
@@ -1,11 +1,55 @@
 #include "dp_algorithms.hpp"
+#include <algorithm>
 
 size_t weighted_interval_scheduling(std::vector<interval> &intervals)
 {
+    void fill_p_table(const std::vector<interval> &intervals, std::vector<int> &ptable);
+    int get_p_value(const std::vector<int> &p_table, const size_t index);
+
     std::sort(intervals.begin(), intervals.end(), [](const interval i1, const interval i2) { 
         return i1.end() < i2.end(); 
     });
 
-    size_t wt_of_selected_intervals = 0;
-    return wt_of_selected_intervals;
+    std::vector<int> p_table(intervals.size(), -1);
+    fill_p_table(intervals, p_table);
+
+    std::vector<size_t> opt_table(intervals.size(), 0);
+    opt_table[0] = intervals[0].weight();
+
+    for (size_t i = 1; i < opt_table.size(); ++i) {
+        size_t compatible_max_opt_entry_till_now = (get_p_value(p_table, i) == -1 ? 0 : opt_table[get_p_value(p_table, i)]);
+        opt_table[i] = std::max(opt_table[i - 1], intervals[i].weight() + compatible_max_opt_entry_till_now);
+    }
+
+    return opt_table[intervals.size() - 1];
+}
+
+void fill_p_table(const std::vector<interval> &intervals, std::vector<int> &p_table)
+{
+    int ceil_binary_search(const std::vector<interval> &intervals, int low, int high, size_t key);
+
+    for (size_t i = 1; i < p_table.size(); ++i) {
+        p_table[i] = ceil_binary_search(intervals, 0, i, intervals[i].start());
+    }
+}
+
+int ceil_binary_search(const std::vector<interval> &intervals, int low, int high, size_t key)
+{
+    if (low <= high) {
+        int mid = ((unsigned int) low + (unsigned int) high) >> 1;
+        if (key == intervals[mid].end()) {
+            return mid;
+        } else if (key < intervals[mid].end()) {
+            return ceil_binary_search(intervals, low, mid - 1, key);
+        } else {
+            return ceil_binary_search(intervals, mid + 1, high, key);
+        }
+    }
+
+    return high;
+}
+
+int get_p_value(const std::vector<int> &p_table, const size_t index)
+{
+    return p_table[index];
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,26 @@ int main(int argc, char *argv[])
 {
     std::vector<interval> intervals;
 
-    intervals.push_back(interval("1", 0, 6, 3));
-    intervals.push_back(interval("2", 1, 4, 5));
-    intervals.push_back(interval("3", 3, 5, 5));
-    intervals.push_back(interval("4", 3, 8, 8));
-    intervals.push_back(interval("5", 4, 7, 3));
-    intervals.push_back(interval("6", 5, 9, 7));
-    intervals.push_back(interval("7", 6, 10, 3));
-    intervals.push_back(interval("8", 8, 11, 4));
+    // intervals.push_back(interval("1", 0, 6, 3));
+    // intervals.push_back(interval("2", 1, 4, 5));
+    // intervals.push_back(interval("3", 3, 5, 5));
+    // intervals.push_back(interval("4", 3, 8, 8));
+    // intervals.push_back(interval("5", 4, 7, 3));
+    // intervals.push_back(interval("6", 5, 9, 7));
+    // intervals.push_back(interval("7", 6, 10, 3));
+    // intervals.push_back(interval("8", 8, 11, 4));
 
-    weighted_interval_scheduling(intervals);
+    intervals.push_back(interval("1", 1, 2, 50));
+    intervals.push_back(interval("2", 3, 5, 20));
+    intervals.push_back(interval("3", 6, 19, 100));
+    intervals.push_back(interval("4", 2, 100, 200));
 
-    for (const interval &i : intervals) {
-        std::cout <<"Id : "<<i.id()<<", Start : "<<i.start()<<", End : "<<i.end()<<", Weight : "<<i.weight()<<std::endl;
-    }
+    size_t max_weight = weighted_interval_scheduling(intervals);
+
+    std::cout <<"Maximum weight of non-overlapping intervals - "<<max_weight<<std::endl;
+    // for (const interval &i : intervals) {
+    //     std::cout <<"Id : "<<i.id()<<", Start : "<<i.start()<<", End : "<<i.end()<<", Weight : "<<i.weight()<<std::endl;
+    // }
 
     return 0;
 }
