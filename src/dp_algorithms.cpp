e796f321c09802ef1e6a9e608f8c707f280ff088
Rajkumar P <raj@diskodev.com>
2017-12-06 14:02:59 +0530

    Change algorithm to compute Weighted Job Scheduling
    Before using the p_table to generate the opt_table (which contains the
    optimal weights until now), the entries are incremented by 1. This is
    because, we can then use the entries in the table as is when computing
    the opt_table.
    
     Changes to be committed:
    	modified:   ../src/dp_algorithms.cpp

--- src/dp_algorithms.cpp
+++ src/dp_algorithms.cpp
@@ -4,7 +4,6 @@
 size_t weighted_interval_scheduling(std::vector<interval> &intervals)
 {
     void fill_p_table(const std::vector<interval> &intervals, std::vector<int> &ptable);
-    int get_p_value(const std::vector<int> &p_table, const size_t index);
 
     std::sort(intervals.begin(), intervals.end(), [](const interval i1, const interval i2) { 
         return i1.end() < i2.end(); 
@@ -12,16 +11,14 @@ size_t weighted_interval_scheduling(std::vector<interval> &intervals)
 
     std::vector<int> p_table(intervals.size(), -1);
     fill_p_table(intervals, p_table);
+    std::for_each(p_table.begin(), p_table.end(), [](int &i) { i += 1; });
 
-    std::vector<size_t> opt_table(intervals.size(), 0);
-    opt_table[0] = intervals[0].weight();
-
+    std::vector<size_t> opt_table(intervals.size() + 1, 0);
     for (size_t i = 1; i < opt_table.size(); ++i) {
-        size_t compatible_max_opt_entry_till_now = (get_p_value(p_table, i) == -1 ? 0 : opt_table[get_p_value(p_table, i)]);
-        opt_table[i] = std::max(opt_table[i - 1], intervals[i].weight() + compatible_max_opt_entry_till_now);
+        opt_table[i] = std::max(opt_table[i - 1], intervals[i - 1].weight() + opt_table[p_table[i - 1]]);
     }
 
-    return opt_table[intervals.size() - 1];
+    return opt_table[opt_table.size() - 1];
 }
 
 void fill_p_table(const std::vector<interval> &intervals, std::vector<int> &p_table)
@@ -47,9 +44,4 @@ int ceil_binary_search(const std::vector<interval> &intervals, int low, int high
     }
 
     return high;
-}
-
-int get_p_value(const std::vector<int> &p_table, const size_t index)
-{
-    return p_table[index];
 }
