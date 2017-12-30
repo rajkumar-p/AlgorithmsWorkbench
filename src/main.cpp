1524e250e806c29d4913169d782876a6ba6ad22e
Rajkumar P <raj@diskodev.com>
2017-12-30 23:10:12 +0530

    Reverse list in-place and in one pass

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <vector>
+#include "list_algorithms.hpp"
 
 int main(int argc, char *argv[])
 {
+    std::vector<int> elements = { 5, 4, 3, 2, 1 };
+
+    ListNode *root = insert_data(elements);
+    for_each_node(root, [](const ListNode *node) {
+        std::cout <<node->val<<"\t";
+    });
+    std::cout <<std::endl;
+
+    ListNode *rev_root = reverse_list(root);
+    for_each_node(rev_root, [](const ListNode *node) {
+        std::cout <<node->val<<"\t";
+    });
+    std::cout <<std::endl;
+
+    delete_list(rev_root);
+
     return 0;
 }
