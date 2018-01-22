1a4ac17e1d84f499f3bc8ca6fd6ab219ab5b3250
Rajkumar P <raj@diskodev.com>
2018-01-22 22:24:17 +0530

    Subset sum algorithm change

--- include/subset_sum_search.hpp
+++ include/subset_sum_search.hpp
@@ -2,6 +2,7 @@
 #define SUBSET_SUM_SEARCH_HPP
 
 #include <vector>
+#include <map>
 #include "quick_sort.hpp"
 #include "binary_search.hpp"
 
@@ -9,24 +10,14 @@ bool subset_sum_search(const std::vector<int> &v, long sum);
 
 bool subset_sum_search(const std::vector<int> &v, long sum)
 {
-    std::vector<int> v_copy(v.begin(), v.end());
-    quick_sort(v_copy, 0, v_copy.size() - 1, std::less<int>());
-
-    for(int i = 0; i < v_copy.size(); ++i) {
-        // v[i] + x = sum => x = sum - v[i];
-        int x = sum - v_copy[i];
-        int found_index = binary_search(v_copy, 0, v_copy.size() - 1, x);
-
-        if (found_index != -1) {
-            if (found_index != i) { return true; }
-            else {
-                int before = i - 1, after = i + 1;
-                if (before >= 0 && v_copy[before] == v_copy[i]) { return true; }
-                if (after < v_copy.size() && v_copy[after] == v_copy[i]) { return true; }
-
-                return false;
-            }
+    std::map<int, bool> numbers_seen;
+    for (size_t i = 0; i < v.size(); ++i) {
+        int to_find = sum - v[i];
+        if (numbers_seen.find(to_find) != numbers_seen.end()) {
+            return true;
         }
+
+        numbers_seen.emplace(v[i], true);
     }
 
     return false;
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <vector>
-#include "string_algorithms.hpp"
+#include <cmath>
 
 int main(int argc, char *argv[])
 {
-    int sum = stoi("22");
-    std::cout <<sum<<std::endl;
+    std::vector<std::string> words = {
+        "organisations", "perhaps", "the", "automation", "monitoring",
+        "orchestration", "skillsets", "are", "of", "extremely", 
+        "then", "I", "components", "state", "expect"
+    };
+
+    std::vector<size_t> words_lengths;
+    for (const std::string &word : words) {
+        words_lengths.push_back(word.length());
+    }
+
+    for (size_t i = 0; i < words.size(); ++i) {
+        std::cout <<words[i]<<" : "<<words_lengths[i]<<std::endl;
+    }
+
     return 0;
 }
